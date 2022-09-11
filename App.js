60910d171ee8164a02ef138101a5dd897737a23d
devruhul <ruhulamin262001@gmail.com>
2022-09-12 02:15:34 +0600

    initialize react native project and playing with react router- native

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link, NativeRouter, Route, Routes } from 'react-router-native';
+import Doctors from './Doctors';
+import Home from './Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello from my React Native</Text>
       <StatusBar style="auto" />
+      {/* set up react native router */}
+      <NativeRouter>
+        <View style={styles.nav}>
+          <Link to="/" >
+            <Text>Home</Text>
+          </Link>
+          <Link to="/doctors" >
+            <Text>Doctors</Text>
+          </Link>
+        </View>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/doctors" element={<Doctors />} />
+        </Routes>
+      </NativeRouter>
     </View>
+
   );
 }
 
